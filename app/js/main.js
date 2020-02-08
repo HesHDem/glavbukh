126e621b2d1195453307891efc18829a3c435cb5
Roman Lyapaev <XilentDem@gmail.com>
2020-02-09 00:26:01 +0300

    add all styles,connect slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,17 @@
-// $(function() {
-// })
+$(function() {
+	$('.slider-min').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 500,
+		// autoplay: true,
+		autoplaySpeed: 3000,
+		infinite: true,
+		swipe: false,
+        accessibility: 'boolean',
+		fade: true,
+		arrows: false,
+		dots: true,
+		dotsClass: 'slider-min__dots',
+		pauseOnDotsHover: true
+	});
+})
